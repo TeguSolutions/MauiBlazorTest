52f424a2ade0bca1f94237680f05003aa6ff2e0b
Istvan Piroska <istvan@tegusolutions.com>
2022-05-18 22:51:01 +0200

    up

--- MauiBlazorTest.Maui/MauiProgram.cs
+++ MauiBlazorTest.Maui/MauiProgram.cs
@@ -12,7 +12,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44371/") });
+        builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:44371/") });
 
 		builder.Services.AddMauiBlazorWebView();
 #if DEBUG
