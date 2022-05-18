fe3f67a291903212dae1bb58363195efdf93d624
Istvan Piroska <istvan@tegusolutions.com>
2022-05-19 01:46:05 +0200

    [Maui Blazor] PersistentComponentStateFix1 test: left as not implemented

--- MauiBlazorTest.Blazor.Components/Pages/PersistentComponentStateFix1.razor.cs
+++ MauiBlazorTest.Blazor.Components/Pages/PersistentComponentStateFix1.razor.cs
@@ -1,7 +1,37 @@
 ﻿using MauiBlazorTest.Blazor.Components.Dev;
+using Microsoft.AspNetCore.Components;
 
 namespace MauiBlazorTest.Blazor.Components.Pages;
 
 public class PersistentComponentStateFix1VM : PageVMBase
 {
+
+
+    #region Lifecycle
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+    }
+
+    #endregion
+
+#if BLAZOR
+    [Inject]
+    protected PersistentComponentState ApplicationState { get; set; }
+
+    public void Test()
+    {
+        ApplicationState.PersistAsJson("bla", "bla");
+        ApplicationState.RegisterOnPersisting(async () => await DoNoting());
+        ApplicationState.TryTakeFromJson("bla", out string instance);
+    }
+
+    private async Task DoNoting()
+    {
+        await Task.Delay(1);
+    }
+#endif
+
+
 }
