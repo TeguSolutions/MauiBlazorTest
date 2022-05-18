f13b8187130d42deb0694e11123879c2c0855e35
Istvan Piroska <istvan@tegusolutions.com>
2022-05-19 01:22:02 +0200

    [Maui Blazor] Components: PersistentComponentState pages added

---
+++ MauiBlazorTest.Blazor.Components/Pages/PersistentComponentStateDefault.razor.cs
@@ -0,0 +1,10 @@
+﻿using MauiBlazorTest.Blazor.Components.Dev;
+using Microsoft.AspNetCore.Components;
+
+namespace MauiBlazorTest.Blazor.Components.Pages;
+
+public class PersistentComponentStateDefaultVM : PageVMBase
+{
+    [Inject]
+    protected PersistentComponentState ApplicationState { get; set; }
+}
---
+++ MauiBlazorTest.Blazor.Components/Pages/PersistentComponentStateFix2.razor.cs
@@ -0,0 +1,7 @@
+﻿using MauiBlazorTest.Blazor.Components.Dev;
+
+namespace MauiBlazorTest.Blazor.Components.Pages;
+
+public class PersistentComponentStateFix2VM : PageVMBase
+{
+}
